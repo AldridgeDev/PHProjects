0f961305c02f449a385f3c68271c0b78dac77c74
AldridgeDev <aldridge.developer@gmail.com>
2017-04-11 16:20:16 -0600

    updating

--- mysql_connect.php
+++ mysql_connect.php
@@ -4,8 +4,24 @@ $hostname = "localhost";
 $username = "root";
 $password = "";
 
-$databaseName = mysql_connect($hostname, $username, $password);
+$databaseName = "phpclass";
+
 $dbConnected  = mysql_connect($hostname, $username, $password);
-$dbSelected   = mysql_select_db($databaseName, $dbConnected)
+$dbSelected   = mysql_select_db($databaseName, $dbConnected);
+
+$dbSuccess = true;
+if ($dbConnected) {
+  if(!dbSelected) {
+    echo "DB connection FAILED<Br>";
+    $dbSuccess = false;
+  }
+} else {
+  echo "MySQL connection FAILED<br>";
+  $dbSuccess = false;
+}
+
+if ($dbSuccess) {
+  
+}
 
  ?>
